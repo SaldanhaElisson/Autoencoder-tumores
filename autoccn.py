4bd351545280acca5c3f9506bcef4d72e7291cf1
SaldanhaElisson <elissonsaldanha19@gmail.com>
2025-01-29 15:30:02 -0300

    add logs

--- autoccn.py
+++ autoccn.py
@@ -4,21 +4,36 @@ from keras.src.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Dense, F
 from keras.src.models import Model
 from keras.src.optimizers import Adam
 from keras.src.utils import to_categorical
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score, recall_score, f1_score
 import os
 
-from  load_images import load_and_augment_images
+from load_images import load_and_augment_images
 
+print("Carregando e aumentando as imagens...")
 base_path = os.path.join(os.getcwd(), 'data_base')
-data, labels = load_and_augment_images(base_path)
-data = data.astype('float32') / 255.0  # Normalize os dados
-data = data.reshape((data.shape[0], 80, 80, 1))
-categories = ["gilioma_tumor", "meningioma_tumor", "no_tumor", "pituitary_tumor"]
-labels_one_hot = to_categorical(labels, num_classes=len(categories))
-x_train, x_test, y_train, y_test = train_test_split(data, labels_one_hot, test_size=0.1, random_state=42)
+x_train, y_train, x_test, y_test = load_and_augment_images(base_path)
+print("Imagens carregadas e aumentadas com sucesso.")
+
+print("Normalizando as imagens...")
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+print("Imagens normalizadas com sucesso.")
+
+print("Redimensionando as imagens...")
+x_train = x_train.reshape((x_train.shape[0], 80, 80, 1))
+x_test = x_test.reshape((x_test.shape[0], 80, 80, 1))
+print("Imagens redimensionadas com sucesso.")
+
+print("Convertendo rótulos para categorias...")
+categories = ["glioma_tumor", "meningioma_tumor", "no_tumor", "pituitary_tumor"]
+y_train = to_categorical(y_train, num_classes=len(categories))
+y_test = to_categorical(y_test, num_classes=len(categories))
+print("Rótulos convertidos com sucesso.")
+
 print(x_train.shape, y_train.shape)
 print(x_test.shape, y_test.shape)
+
+print("Construindo o modelo do autoencoder...")
 input_img = Input(shape=(80, 80, 1))
 
 x = Conv2D(32, (2, 2), activation='relu', padding='same')(input_img)
@@ -53,11 +68,16 @@ x = BatchNormalization()(x)
 decoded = Conv2D(1, (2, 2), activation='sigmoid', padding='same')(x)
 
 autoencoder = Model(input_img, decoded)
-
 autoencoder.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy')
+print("Modelo do autoencoder construído e compilado com sucesso.")
 
+
+print("Treinando o autoencoder...")
 history_autoencoder = autoencoder.fit(x_train, x_train, epochs=100, batch_size=16, shuffle=True, validation_data=(x_test, x_test))
+print("Autoencoder treinado com sucesso.")
 
+
+print("Construindo o modelo do classificador...")
 encoder = Model(inputs=input_img, outputs=autoencoder.get_layer('conv2d_5').output)
 
 encoded_input = Input(shape=(20, 20, 64))
@@ -72,11 +92,15 @@ x = Dropout(0.5)(x)
 x = Dense(4, activation='softmax')(x)
 
 classifier = Model(encoded_input, x)
-
 classifier.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+print("Modelo do classificador construído e compilado com sucesso.")
 
+
+print("Treinando o classificador...")
 history_classifier = classifier.fit(encoder.predict(x_train), y_train, epochs=100, batch_size=16, shuffle=True, validation_data=(encoder.predict(x_test), y_test))
+print("Classificador treinado com sucesso.")
 
+print("Avaliando o classificador...")
 y_pred = classifier.predict(encoder.predict(x_test))
 y_pred_class = np.argmax(y_pred, axis=1)
 
@@ -92,7 +116,8 @@ print("Recall:", recall)
 print("Sensitividade:", sensitividade)
 print("F-measure:", f_measure)
 
-
+# Plotar as curvas de aprendizagem
+print("Plotando as curvas de aprendizagem...")
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
 plt.plot(history_autoencoder.history['loss'], label='Loss Treinamento')
@@ -112,3 +137,4 @@ plt.legend()
 
 plt.tight_layout()
 plt.show()
+print("Curvas de aprendizagem plotadas com sucesso.")
--- load_images.py
+++ load_images.py
@@ -9,22 +9,38 @@ def augment_image(image):
 
 def load_and_augment_images(base_path):
     categorias = ["glioma_tumor", "meningioma_tumor", "no_tumor", "pituitary_tumor"]
-    data = []
-    labels = []
+    train_data = []
+    train_labels = []
+    test_data = []
+    test_labels = []
 
     for categoria in categorias:
-        path = os.path.join(base_path, categoria)
+        train_path = os.path.join(base_path, 'Training', categoria)
+        test_path = os.path.join(base_path, 'Testing', categoria)
         class_num = categorias.index(categoria)
-        for img in os.listdir(path):
+
+        for img in os.listdir(train_path):
             try:
-                img_path = os.path.join(path, img)
+                img_path = os.path.join(train_path, img)
                 image = Image.open(img_path).convert('L')
                 image = image.resize((80, 80))
                 augmented_images = augment_image(image)
                 for augmented_image in augmented_images:
-                    data.append(np.array(augmented_image))
-                    labels.append(class_num)
+                    train_data.append(np.array(augmented_image))
+                    train_labels.append(class_num)
             except Exception as e:
-                print("Erro ao carregar a imagem {img_path}: {e}".format(img_path=img_path, e=e))
+                print(f"Erro ao carregar a imagem {img_path}: {e}")
 
-    return np.array(data), np.array(labels)
+        for img in os.listdir(test_path):
+            try:
+                img_path = os.path.join(test_path, img)
+                image = Image.open(img_path).convert('L')
+                image = image.resize((80, 80))
+                augmented_images = augment_image(image)
+                for augmented_image in augmented_images:
+                    test_data.append(np.array(augmented_image))
+                    test_labels.append(class_num)
+            except Exception as e:
+                print(f"Erro ao carregar a imagem {img_path}: {e}")
+
+    return np.array(train_data), np.array(train_labels), np.array(test_data), np.array(test_labels)
